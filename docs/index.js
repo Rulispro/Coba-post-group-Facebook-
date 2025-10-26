f9999db806970bca98650774f8558942aec2474e
Rulispro <95007142+Rulispro@users.noreply.github.com>
2025-10-27 06:27:03 +0700

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -415,7 +415,7 @@ console.log("FILL:", fillResult);
   // ===== 3️⃣ Download + upload media
  const today = process.env.DATE;
  const fileName =  `akun1_${today}.png`;// bisa .mp4
-const mediaUrl ="https://github.com/Rulispro/Coba-post-group-Facebook-/releases/download/V1.0/Screenshot_20250909-071607.png";
+const mediaUrl ="https://github.com/Rulispro/Coba-post-group-Facebook-/releases/download/V1.0/Screenshot_20251013-115539.png";
 // download media → simpan return value ke filePat
   const filePath = await downloadMedia(mediaUrl, fileName);
 console.log(`✅ Media ${fileName} berhasil di-download.`);
