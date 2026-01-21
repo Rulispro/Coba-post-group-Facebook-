cb2af2b7cafa6c1fb5d215bf6b195c794829fbb9
Rulispro <95007142+Rulispro@users.noreply.github.com>
2026-01-21 14:27:07 +0700

    Update accounts.js

--- docs/accounts.js
+++ docs/accounts.js
@@ -1,1 +1,81 @@
+[
+  {
+    "account": "akun1",
+    "cookies": [ {
+  "account": "akun1",
+  "cookies": [
+    { "name": "datr", "value": "xN8taKSRj14g2OqdDJRuQsTh", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "sb", "value": "xN8taLnQjBtn7csMtQswlPqw", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "locale", "value": "id_ID", "domain": ".facebook.com", "path": "/" },
+    { "name": "ps_l", "value": "1", "domain": ".facebook.com", "path": "/" },
+    { "name": "ps_n", "value": "1", "domain": ".facebook.com", "path": "/" },
+    { "name": "m_pixel_ratio", "value": "2", "domain": ".facebook.com", "path": "/" },
+    { "name": "vpd", "value": "v1%3B687x360x2", "domain": ".facebook.com", "path": "/" },
+    { "name": "dpr", "value": "2", "domain": ".facebook.com", "path": "/" },
+    { "name": "wd", "value": "360x825", "domain": ".facebook.com", "path": "/" },
+    { "name": "c_user", "value": "61576156023619", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "xs", "value": "16%3A10xTSk5_US5zpw%3A2%3A1768979558%3A-1%3A-1", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "fr", "value": "0RQn1oMPRkO0lIoXx.AWd9MR6OeDgODoXjJBeouviHn9P13UsebdJ1hv83Ojz1ADlYGDs.BoLd_E..AAA.0.0.BpcHxs.AWdSOjaFM-BIFNvvA93uCFheQLI", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "pas", "value": "61576001021086%3AUQYJ9AuaM7%2C61576156023619%3A0XtatjXJiO", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "fbl_st", "value": "101724305%3BT%3A29482992", "domain": ".facebook.com", "path": "/" },
+    { "name": "wl_cbv", "value": "v2%3Bclient_version%3A3058%3Btimestamp%3A1768979564", "domain": ".facebook.com", "path": "/" }
+  ],
+  "caption": "Halo grup 👋",
+  "mediaUrl": "https://....jpg",
+  "groups": [
+    "https://m.facebook.com/groups/111",
+    "https://m.facebook.com/groups/222"
+  ],
+  "schedule": "2026-01-21",
+  "time": "09:00"
+}
+ ],
+    "caption": "Halo grup 👋",
+    "mediaUrl": "https://....jpg",
+    "groups": [
+      "https://m.facebook.com/groups/111",
+      "https://m.facebook.com/groups/222"
+    ],
+    "schedule": "2026-01-21",
+    "time": "09:00"
+  },
+  {
+    "account": "akun2",
+    "cookies": [ {
+  "account": "akun2",
+  "cookies": [
+    { "name": "datr", "value": "xN8taKSRj14g2OqdDJRuQsTh", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "sb", "value": "xN8taLnQjBtn7csMtQswlPqw", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "locale", "value": "id_ID", "domain": ".facebook.com", "path": "/" },
+    { "name": "ps_l", "value": "1", "domain": ".facebook.com", "path": "/" },
+    { "name": "ps_n", "value": "1", "domain": ".facebook.com", "path": "/" },
+    { "name": "m_pixel_ratio", "value": "2", "domain": ".facebook.com", "path": "/" },
+    { "name": "vpd", "value": "v1%3B687x360x2", "domain": ".facebook.com", "path": "/" },
+    { "name": "dpr", "value": "2", "domain": ".facebook.com", "path": "/" },
+    { "name": "wd", "value": "360x825", "domain": ".facebook.com", "path": "/" },
+    { "name": "c_user", "value": "61576977154304", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "xs", "value": "2%3A_cN73ZhnzUuziw%3A2%3A1768979694%3A-1%3A-1", "domain": ".facebook.com", "path": "/", "secure": true, "httpOnly": true },
+    { "name": "fr", "value": "0RQn1oMPRkO0lIoXx.AWcJoFnWUxuhsEoKGg9cUSsaOW72aEudGW2Gwf82ekhrc06J2U8.BoLd_E..AAA.0.0.BpcHzx.AWdiai5iOhgFWcGDzwmn6rjxvzQ", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "pas", "value": "61576001021086%3AUQYJ9AuaM7%2C61576977154304%3AduA8yBMfeh", "domain": ".facebook.com", "path": "/", "secure": true },
+    { "name": "fbl_st", "value": "101638233%3BT%3A29482994", "domain": ".facebook.com", "path": "/" },
+    { "name": "wl_cbv", "value": "v2%3Bclient_version%3A3058%3Btimestamp%3A1768979697", "domain": ".facebook.com", "path": "/" }
+  ],
+  "caption": "Caption akun 2",
+  "mediaUrl": "https://....mp4",
+  "groups": [
+    "https://m.facebook.com/groups/333"
+  ],
+  "schedule": "2026-01-21",
+  "time": "15:00"
+}
+ ],
+    "caption": "Caption akun 2",
+    "mediaUrl": "https://....mp4",
+    "groups": [
+      "https://m.facebook.com/groups/333"
+    ],
+    "schedule": "2026-01-21",
+    "time": "15:00"
+  }
+]
 
